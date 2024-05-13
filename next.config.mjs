e910b8a8e0dd0d55cbbba2bbcdcf7d21329d0ec7
Roshan-singh-yadav <167501032+Roshan-singh-yadav@users.noreply.github.com>
2024-05-13 13:46:47 +0000

    remove

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    // basePath: "/2048-in-react", // base dir name
-    // output: "export",  // <=== enables static exports
-    // reactStrictMode: true,
-};
-
-export default nextConfig;
